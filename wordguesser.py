4a1a826e831a40f83dba26b9c339aedbe4a632ea
tanviiir04 <tanvirastogi1911@gmail.com>
2025-05-08 11:06:45 +0400

    Initial commit - Word Guesser

---
+++ wordguesser.py
@@ -0,0 +1,25 @@
+import random
+
+word_bank = ['elephant', 'alligator', 'monkey', 'shark', 'giraffe']
+word = random.choice(word_bank)
+guessedword = ['_'] * len(word)
+attempts = 10
+
+while attempts > 0:
+    print('\nCurrent word: ' + ''.join(guessedword))
+    guess = input("Guess a letter: ").lower()
+
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedword[i] = guess
+        print("Great guess!")
+    else:
+        attempts -= 1
+        print("Wrong guess! Attempts left: " + str(attempts))
+
+    if '_' not in guessedword:
+        print("\nCongratulations! The word is: " + word)
+        break
+else:
+    print("\nYou've run out of attempts! The word was: " + word)
